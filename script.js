2455781ef96d2430918bc6f095ebefe6cb4b452c
annswiderek <122670755+annswiderek@users.noreply.github.com>
2023-01-25 21:03:04 +0100

    2

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ function myColorChange() {
     const element = document.getElementById("id01");
     element.style.color = "red";
 }
+
+
+
+
+
+function mojaFunkcja() {
+
+    const target = 4;
+    let sum = 0;
+  
+    for (let i = 0; i < target; i += 1) {
+      console.log("Twoje i wynosi: " + i);
+      console.log("Obliczenie: " + sum + " + " + i + " =");
+      sum += i;
+      console.log("Wynik: " + sum);
+    }
+    console.log(sum);
+  
+  }
+  const clients = ["Mango", "Poly", "Ajax", "Ajax"];
+
+    for (let i = 0; i < clients.length; i += 1) { 
+        console.log(clients[i]); 
+    }
