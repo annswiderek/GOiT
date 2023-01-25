40fa2eec8e514264cb1bb4d116b06af7db0ab26f
annswiderek <122670755+annswiderek@users.noreply.github.com>
2023-01-25 19:38:32 +0100

    lol
    lol

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    const element = document.getElementById("id01");
+    element.innerHTML = "New Heading";
+}
+function myColorChange() {
+    const element = document.getElementById("id01");
+    element.style.color = "red";
+}
