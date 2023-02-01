a9d13b584b333607bf97e564de232f661acae4b0
annswiderek <122670755+annswiderek@users.noreply.github.com>
2023-02-01 19:20:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,44 @@
 function myFunction() {
-    const element = document.getElementById("id01");
-    element.innerHTML = "New Heading";
+  const element = document.getElementById("id01");
+  element.innerHTML = "New Heading";
 }
 function myColorChange() {
-    const element = document.getElementById("id01");
-    element.style.color = "red";
+  const element = document.getElementById("id01");
+  element.style.color = "red";
 }
 
+const przesylka = {
+  name: "",
+  ulica: "",
+};
+const przesylka2 = {
+  name: "",
+  ulica: "",
+};
 
+przesylka2.waga = 20;
+przesylka.name = "List";
+przesylka2.name = "Paczka";
+function mojaFunkcja(id) {
 
+  const element = document.getElementById("id01");
+  const element2 = document.getElementById("id02");
+  if (id === 1) { //instrukcje dla przycisku z informacja o przesylce 1
 
-
-function mojaFunkcja() {
-
-    const target = 4;
-    let sum = 0;
-  
-    for (let i = 0; i < target; i += 1) {
-      console.log("Twoje i wynosi: " + i);
-      console.log("Obliczenie: " + sum + " + " + i + " =");
-      sum += i;
-      console.log("Wynik: " + sum);
-    }
-    console.log(sum);
-  
+    informacja = "Typ twojej przesylki to: " + przesylka.name;
+    element.innerHTML = informacja;
+    
+    informacja = "Przesyłka nie posiada wagi";
+    element2.innerHTML = informacja;
   }
-  const clients = ["Mango", "Poly", "Ajax", "Ajax"];
 
-    for (let i = 0; i < clients.length; i += 1) { 
-        console.log(clients[i]); 
-    }
+  else if (id === 2) { //instrukcje dla przycisku z informacja o przesylce 2
+
+    informacja = "Typ twojej przesylki to: " + przesylka2.name;
+    element.innerHTML = informacja;
+  
+    informacja = "Waga twojej przesylki to: " + przesylka2.waga + " kg";
+    element2.innerHTML = informacja;
+  }
+
+}
